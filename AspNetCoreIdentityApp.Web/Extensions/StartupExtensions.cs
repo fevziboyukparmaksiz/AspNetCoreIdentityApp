3f4418348c6d7e3fad9a31a741eb849c8f73bece
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-01 01:01:51 +0300

    Password Validation Options

---
+++ AspNetCoreIdentityApp.Web/Extensions/StartupExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+
+namespace AspNetCoreIdentityApp.Web.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddIdentityWithExt(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuwxyz1234567890_";
+
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = true;
+
+
+
+            }).AddEntityFrameworkStores<AppDbContext>();
+        }
+    }
+}
