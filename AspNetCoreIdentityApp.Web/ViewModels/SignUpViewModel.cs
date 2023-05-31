94ef0e706f1e0fe62eebcea0da69717a6fc715a9
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-01 00:57:10 +0300

    Client-Side Validations

--- AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -4,14 +4,25 @@ namespace AspNetCoreIdentityApp.Web.ViewModels
 {
     public class SignUpViewModel
     {
-        [Display(Name = "Kullanıcı Adı: ")]
+        [Required(ErrorMessage = "Kullanıcı Ad alanı boş bırakılamaz!")]
+        [Display(Name = "Kullanıcı adı :")]
         public string? UserName { get; set; }
-        [Display(Name = "Email : ")]
+
+        [EmailAddress(ErrorMessage = "Email formatı yanlıştır.")]
+        [Required(ErrorMessage = "Email alanı boş bırakılamaz!")]
+        [Display(Name = "Email :")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon alanı boş bırakılamaz!")]
         [Display(Name = "Telefon :")]
         public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı boş bırakılamaz!")]
         [Display(Name = "Şifre :")]
         public string? Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Şifreler aynı değildir.")]
+        [Required(ErrorMessage = "Şifre Tekrar alanı boş bırakılamaz!")]
         [Display(Name = "Şifre Tekrar :")]
         public string? PasswordConfirm { get; set; }
     }
