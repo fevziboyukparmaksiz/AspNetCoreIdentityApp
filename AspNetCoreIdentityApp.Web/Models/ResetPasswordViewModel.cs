aba3a01afe6bd5cdd8e81e99d9b3121ff6ac1833
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-03 23:46:50 +0300

    sifremi unuttum sayfasının oluşturulması
    şifremi unuttum sayfasının oluşturulması

---
+++ AspNetCoreIdentityApp.Web/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AspNetCoreIdentityApp.Web.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage = "Email formatı yanlıştır.")]
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+    }
+}
