1c9529af3c31221c096d6ab0a35d58761a9c4c76
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-06 16:37:10 +0300

    Authorization / Rol oluşturma-1

---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager=roleManager;
+            _userManager=userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
