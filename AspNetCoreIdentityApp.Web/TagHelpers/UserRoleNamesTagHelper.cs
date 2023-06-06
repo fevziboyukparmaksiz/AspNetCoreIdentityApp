85e4fdbaf8c9d0728ccb20d5c6c1b7889978c917
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-06 22:57:14 +0300

    Role Atama 3.kısım

---
+++ AspNetCoreIdentityApp.Web/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+
+namespace AspNetCoreIdentityApp.Web.TagHelpers
+{
+    public class UserRoleNamesTagHelper : TagHelper
+    {
+        public string UserId { get; set; } = null!;
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleNamesTagHelper(UserManager<AppUser> userManager)
+        {
+            _userManager=userManager;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var user = await _userManager.FindByIdAsync(UserId);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var stringBuilder = new StringBuilder();
+
+            userRoles.ToList().ForEach(x =>
+            {
+                stringBuilder.Append($"<h6><span class='badge bg-secondary'>{x.ToLower()}</span></h6>");
+            });
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+
+        }
+    }
+}
