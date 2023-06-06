f2ab33bc5a82ee51ee9362c9f3099a5ada849db1
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-06 18:13:24 +0300

    Rol listeleme

--- AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RoleController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RoleController.cs
@@ -3,6 +3,7 @@ using AspNetCoreIdentityApp.Web.Extensions;
 using AspNetCoreIdentityApp.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
 {
@@ -18,9 +19,18 @@ namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
             _userManager=userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var roles = await _roleManager.Roles.Select(x => new RoleListViewModel
+            {
+                Id = x.Id,
+                Name= x.Name
+            })
+            .ToListAsync();
+
+
+
+            return View(roles);
         }
 
         public IActionResult RoleCreate()
