49823a460305c34b62218c6f12ae1f0a5380b3b0
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-06 20:28:21 +0300

    Rol Silme

--- AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RoleController.cs
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RoleController.cs
@@ -46,6 +46,7 @@ namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
                 return View();
             }
 
+            TempData["SuccessMessage"] = "Role başarıyla oluşturulmuştur.";
 
             return RedirectToAction(nameof(RoleController.Index));
         }
@@ -75,13 +76,29 @@ namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
             await _roleManager.UpdateAsync(roleToUpdate);
 
             ViewData["SuccessMessage"] = "Rol bilgisi güncellenmiştir";
-            {
-
-            }
 
             return View();
         }
 
+        public async Task<IActionResult> RoleDelete(string id)
+        {
+            var roleToDelete = await _roleManager.FindByIdAsync(id);
+            if (roleToDelete == null)
+            {
+                throw new Exception("Güncellenecek rol bulunamadı.");
+            }
 
+            var result = await _roleManager.DeleteAsync(roleToDelete);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(result.Errors.Select(x => x.Description).First());
+
+            }
+
+            TempData["SuccessMessage"] = "Role başarıyla silinmiştir.";
+
+            return RedirectToAction(nameof(RoleController.Index));
+        }
     }
 }
