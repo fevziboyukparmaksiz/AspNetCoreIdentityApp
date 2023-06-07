3b3044e4772fe3cc9df0386199c333f203626cae
Fevzi Böyükparmaksız <fevzibparmaksiz@gmail.com>
2023-06-07 22:19:33 +0300

    2.Senaryo ( Bir sayfaya belirli bir süre erişmek) - AuthorizationHandler

---
+++ AspNetCoreIdentityApp.Web/Requirements/ExchangeExpireRequirement.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace AspNetCoreIdentityApp.Web.Requirements
+{
+    public class ExchangeExpireRequirement : IAuthorizationRequirement
+    {
+
+    }
+
+    public class ExchangeExpireRequirementHandler : AuthorizationHandler<ExchangeExpireRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ExchangeExpireRequirement requirement)
+        {
+
+            if (!context.User.HasClaim(x => x.Type == "ExhangeExpireDate"))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var exchangeExpireDate = context.User.FindFirst("ExhangeExpireDate")!;
+
+            if (DateTime.Now > Convert.ToDateTime(exchangeExpireDate.Value))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+
+        }
+    }
+}
